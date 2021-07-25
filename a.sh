ae55e68cc71f27dd29594dbace9c2fb7cd16f051
onevirus <onevirus@gmail.com>
2021-07-25 19:10:08 +0900

    a

--- a.sh
+++ a.sh
@@ -1,3 +1,8 @@
+[ -f /proc/net/if_inet6 ] && echo 'IPv6 ready system!' || echo 'No IPv6 support found! Compile the kernel!!'
+
+lsmod | grep -qw ipv6 && echo "IPv6 kernel driver loaded and configured." || echo "IPv6 not configured and/or driver loaded on the system."
+
+
 curl -vv --ipv6 "[2607:f8b0:4002:c03::93]:80"
 
 url=('https://ident.me' 'https://api.ipify.org')
