6e6ac2a3a02b5957aba353f297f1b87acaa37b88
onevirus <onevirus@gmail.com>
2021-07-25 18:41:24 +0900

    a

---
+++ a.sh
@@ -0,0 +1,11 @@
+url=('https://ident.me' 'https://api.ipify.org')
+
+for u in ${url[@]}; do
+  curl -6 -s -m 2 $u && break
+done
+
+echo "---"
+
+for u in ${url[@]}; do
+  curl -4 -s -m 2 $u && break
+done
